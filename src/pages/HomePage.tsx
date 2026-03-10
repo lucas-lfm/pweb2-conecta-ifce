e3fa9d824eabc1238a3e96f1e76ae8752c730d8b
Lupercio <lupercionetof@gmail.com>
2026-03-10 17:36:56 -0300

    feat: CtaSectoin and FaqSection implementation and their components

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,15 @@
 import FeatureSection from "@/components/homepage/feature-section"
 import HeroSection from "@/components/homepage/hero-section"
+import CtaSection from "@/components/homepage/cta-section"
+import FaqSection from "@/components/homepage/faq-section"
 
 function HomePage() {
   return (
     <>
       <HeroSection />
       <FeatureSection />
+      <FaqSection />
+      <CtaSection />
     </>
   )
 }
