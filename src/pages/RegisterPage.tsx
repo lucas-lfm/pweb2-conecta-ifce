1b1a7a109adecf6e485c17cbdd99f62c5e93a197
Lupercio <lupercionetof@gmail.com>
2026-03-10 20:48:05 -0300

    feat: Form management with React Hook Form library

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -4,32 +4,27 @@ import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter }
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { registerSchema } from "@/schemas/register.schema"
+import { registerSchema, type RegisterFormData } from "@/schemas/register.schema"
 import { EyeIcon, EyeOffIcon } from "lucide-react"
 import { useState } from "react"
-import { ZodError } from "zod"
+import { useForm, Controller } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
 
 function RegisterPage() {
   const [showPass, setShowPass] = useState<boolean>(false)
 
-  const handleSubmit = (event: React.SubmitEvent) => {
-    event.preventDefault()
-    const formData = new FormData(event.target)
+  const { register, handleSubmit, reset, control, formState: { errors, isSubmitting, isValid } } = useForm<RegisterFormData>({
+    resolver: zodResolver(registerSchema),
+    mode: 'onBlur'
+  })
 
-    const data = {
-      firstName: formData.get('firstName'),
-      password: formData.get('password')
-    }
+  const onSubmit = async (data: RegisterFormData) => {
+    console.log('Enviando...', data)
 
-    try {
-      const validData = registerSchema.parse(data)
-      console.log(validData)
-    } catch (error) {
-      if (error instanceof ZodError) {
-        console.log(error)
-      }
-    }
+    await new Promise(resolve => setTimeout(resolve, 2000))
 
+    console.log('Usuário Cadastrado')
+    reset()
   }
 
   return (
@@ -51,77 +46,113 @@ function RegisterPage() {
         </CardHeader>
 
         <CardContent>
-          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
             <div className="flex items-center gap-4">
               <div className="flex flex-col gap-2">
                 <Label htmlFor="firstName" className="text-foreground">Nome</Label>
                 <Input
-                id="firstName" name="firstName" type="text"
+                id="firstName" type="text"
                 placeholder="Seu nome" required
-                className="h-11 bg-background"/>
+                className="h-11 bg-background"
+                {...register('firstName')} />
+
+                {errors.firstName && (
+                  <p className="text-xs text-destructive">
+                    {errors.firstName.message}
+                  </p>
+                )}
               </div>
 
               <div className="flex flex-col gap-2">
                 <Label htmlFor="lastName" className="text-foreground">Sobrenome</Label>
                 <Input
-                id="lastName" name="lastName" type="text"
+                id="lastName" type="text"
                 placeholder="Seu sobrenome"
-                className="h-11 bg-background"/>
+                className="h-11 bg-background"
+                {...register('lastName')}/>
+
+                {errors.lastName && (
+                  <p className="text-xs text-destructive">
+                    {errors.lastName.message}
+                  </p>
+                )}
               </div>
             </div>
 
             <div className="flex flex-col gap-2">
               <Label htmlFor="email" className="text-foreground">E-mail Institucional</Label>
               <Input
-              id="email" name="email" type="email"
+              id="email" type="email"
               placeholder="seu.nome@ifce.edu.br"
-              className="h-11 bg-background"/>
+              className="h-11 bg-background"
+              {...register('email')}/>
+
+              {errors.email && (
+                <p className="text-xs text-destructive">
+                  {errors.email.message}
+                </p>
+              )}
             </div>
 
             <div className="flex flex-col gap-2">
               <Label htmlFor="role" className="text-foreground">
                 Vínculo
               </Label>
+              <Controller name="role" control={control} render={({ field }) => (
+                <Select onValueChange={field.onChange} value={field.value ?? ""}>
+                  <SelectTrigger id="role" className="bg-background w-full">
+                    <SelectValue placeholder="Selecione seu vínculo com o IFCE"/>
+                  </SelectTrigger>
 
-              <Select>
-                <SelectTrigger id="role" className="bg-background w-full">
-                  <SelectValue placeholder="Selecione seu vínculo com o IFCE"/>
-                </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="student">Estudante</SelectItem>
+                    <SelectItem value="professor">Docente</SelectItem>
+                    <SelectItem value="technician">Técnico(a)</SelectItem>
+                  </SelectContent>
+                </Select>
+              )}/>
 
-                <SelectContent>
-                  <SelectItem value="student">Estudante</SelectItem>
-                  <SelectItem value="professor">Docente</SelectItem>
-                  <SelectItem value="technician">Técnico(a)</SelectItem>
-                </SelectContent>
-              </Select>
+              {errors.role && (
+                <p className="text-xs text-destructive">
+                  {errors.role.message}
+                </p>
+              )}
             </div>
 
             <div className="flex flex-col gap-2">
               <Label htmlFor="campus" className="text-foreground">
                 Campus
               </Label>
+              <Controller name="campus" control={control} render={({ field }) => (
+                <Select onValueChange={field.onChange} value={field.value ?? ""}>
+                  <SelectTrigger id="campus" className="bg-background w-full">
+                    <SelectValue placeholder="Selecione seu Campus"/>
+                  </SelectTrigger>
 
-              <Select>
-                <SelectTrigger id="campus" className="bg-background w-full">
-                  <SelectValue placeholder="Selecione seu Campus"/>
-                </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="taua">Tauá</SelectItem>
+                    <SelectItem value="boa_viagem">Boa Viagem</SelectItem>
+                    <SelectItem value="fortaleza">Fortaleza</SelectItem>
+                  </SelectContent>
+                </Select>
+              )}/>
 
-                <SelectContent>
-                  <SelectItem value="student">Tauá</SelectItem>
-                  <SelectItem value="professor">Boa Viagem</SelectItem>
-                  <SelectItem value="technician">Fortaleza</SelectItem>
-                </SelectContent>
-              </Select>
+              {errors.campus && (
+                <p className="text-xs text-destructive">
+                  {errors.campus.message}
+                </p>
+              )}
             </div>
 
 
             <div className="flex flex-col gap-2">
               <Label htmlFor="password" className="text-foreground">Senha</Label>
               <div className="relative">
-                <Input id="password" name="password" type={ showPass ? "text" : "password"}
+                <Input id="password" type={ showPass ? "text" : "password"}
                   placeholder="Digite sua senha"
                   required
                   className="h-11 bg-background"
+                  {...register('password')}
                 />
 
                 <button
@@ -134,6 +165,12 @@ function RegisterPage() {
                 </button>
               </div>
 
+              {errors.password && (
+                <p className="text-xs text-destructive">
+                  {errors.password.message}
+                </p>
+              )}
+
               <p className="text-xs text-foreground">Mínimo de 8 caracteres com letras e números</p>
             </div>
 
@@ -142,7 +179,7 @@ function RegisterPage() {
         </CardContent>
 
         <CardFooter className="border-t border-border">
-          <p className="text-sm text-muted-foreground text-center w-full">Já tem conta? <a href="/login"></a>Entrar</p>
+          <p className="text-sm text-muted-foreground text-center w-full">Já tem conta? <a href="/login">Entrar</a></p>
         </CardFooter>
       </Card>
     </section>
