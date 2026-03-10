0b3ea1d841c5d29c3eac408563b6b39eca17256f
Lupercio <lupercionetof@gmail.com>
2026-03-10 17:15:19 -0300

    feat: feature section implementation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,12 @@
+import FeatureSection from "@/components/homepage/feature-section"
 import HeroSection from "@/components/homepage/hero-section"
 
 function HomePage() {
   return (
-    <HeroSection />
+    <>
+      <HeroSection />
+      <FeatureSection />
+    </>
   )
 }
 
