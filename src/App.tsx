648c4e94d6ff984d4fe5737e53acf523ddf9c88c
pauloCosmo55 <pauloclarentino85@gmail.com>
2026-03-25 11:51:59 -0300

    feat: adiciona componente Footer e atualiza App para utilizá-lo

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-
 import Navbar from "./components/shared/navbar";
+import { Footer } from "./components/shared/footer"; // Importação nova
 
 function App() {
-
-
   return (
-    <>
     <div className="flex flex-col min-h-svh">
       <Navbar />
-      <main className="flex-1">Conteúdo Principal</main>
-      <footer>Rodapé</footer>
+      <main className="flex-1 container mx-auto px-4 py-8">
+        {/* O conteúdo das próximas aulas entrará aqui */}
+        <h1 className="text-2xl font-bold">Conteúdo Principal</h1>
+      </main>
+      <Footer /> {/* Substituímos a tag footer simples pelo componente */}
     </div>
-    </>
   );
 }
 
 export default App;
-
-
