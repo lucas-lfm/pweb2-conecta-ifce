affdd75e27b5827e935b126b4cc695a1d763142e
pauloCosmo55 <pauloclarentino85@gmail.com>
2026-03-26 23:54:35 -0300

    feat: remove AuthProvider e RouterProvider; reestrutura App para incluir Navbar, HomePage e Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { AuthProvider } from '@/features/auth/contexts/AuthContext'
-import { router } from '@/routes/router'
-import { RouterProvider } from 'react-router'
+import Navbar from '@/components/shared/navbar'
+import { Footer } from '@/components/shared/footer'
+import HomePage from '@/pages/HomePage'
 
 function App() {
   return (
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <div className="flex flex-col min-h-svh">
+      <Navbar />
+      <main className="flex-1 container mx-auto px-4 py-8">
+        <HomePage />
+      </main>
+      <Footer />
+    </div>
   )
 }
 
