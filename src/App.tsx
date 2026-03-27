7fc6aa41663be3f93d6109db64170cff1f3eadfc
pauloCosmo55 <pauloclarentino85@gmail.com>
2026-03-27 01:23:18 -0300

    feat: ajusta estrutura do App para utilizar Outlet e corrige classe min-h-screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import Navbar from '@/components/shared/navbar'
-import { Footer } from '@/components/shared/footer'
-import HomePage from '@/pages/homepage/HomePage'
+import { Outlet } from 'react-router-dom'
+import { Navbar } from './components/shared/navbar'
+import { Footer } from './components/shared/footer'
 
 function App() {
   return (
-    <div className="flex flex-col min-h-svh">
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-1 container mx-auto px-4 py-8">
-        <HomePage />
+      <main className="flex-1 flex flex-col">
+        {/* O Outlet é onde as páginas (Home, Login, etc) vão "aparecer" */}
+        <Outlet />
       </main>
       <Footer />
     </div>
